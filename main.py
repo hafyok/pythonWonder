2a3b944a0ae09265000e58f1bf07c91d6135b63a
Haggai Khafizov <hafyok5777@gmail.com>
2022-10-21 23:51:47 +0300

    Output of first letter

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 class Wonder:
     words_all = ["Чудо", "Поле"]
+    word = ""
+    letter = " "
 
     def __init__(self, word):
         len_word = len(word)
-        print(len_word)
-
-        def empty_cell(self, len_cell):
-            for i in range(len_cell):
-                print("___ ", end="")
-            print("")
-            for i in range(len_cell):
-                print("| | ", end="")
-            print("")
-            for i in range(len_cell):
-                print("--- ", end="")
-
-        empty_cell(self, len_word)
+        self.word = word
+        self.empty_cell(len_word)
 
 
-o = Wonder("чудоkkk")
+    def empty_cell(self, len_cell):
+        for i in range(len_cell):
+            print("___ ", end="")
+        print("")
+        for i in range(len_cell):
+            print("|" + self.letter + "| ", end="")
+        print("")
+        for i in range(len_cell):
+            print("--- ", end="")
+        print("")
+
+
+
+    def input_letter(self):
+        letter = input()
+        if letter == self.word[0]:
+            self.letter = letter
+            self.empty_cell(len(self.word))
+            print("верно")
+        else:
+            print("неверно")
+
+
+
+
+
+o = Wonder("Чудо")
+o.input_letter()
