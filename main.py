e6f605df28be189e02190b5d3d33c2683ad65d0b
Haggai Khafizov <hafyok5777@gmail.com>
2022-10-21 23:25:15 +0300

    Displaying the number of letters on the screen

---
+++ main.py
@@ -0,0 +1,11 @@
+class Wonder:
+    words_all = ["Чудо", "Поле"]
+
+    def __init__(self, word):
+        len_word = len(word)
+        print(len_word)
+
+    # def empty_cell(self, len_cell):
+    #     for i in range()
+
+o = Wonder("чудо")
